f9f71ea60f320ded506a3b553a8f23d24af14c34
blivesta <enmt@blivesta.com>
2015-07-26 03:08:20 +0900

    remove, var root

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,5 @@
 'use strict';
 
-var browserSync = require('browser-sync');
-var browserify = require('browserify');
-var del = require('del');
-var fs = require('fs');
-var cssnext = require('gulp-cssnext');
-var data = require('gulp-data');
-var fm = require('gulp-front-matter');
-var gulp = require('gulp');
-var header = require('gulp-header');
-var imagemin = require('gulp-imagemin');
-var jshint = require('gulp-jshint');
-var rename = require('gulp-rename');
-var sourcemaps   = require('gulp-sourcemaps');
-var stylestats = require('gulp-stylestats');
-var uglify = require('gulp-uglify');
-var pngquant = require('imagemin-pngquant');
-var yaml = require('js-yaml');
-var stylish = require('jshint-stylish');
-var runSequence = require('run-sequence');
-var source = require('vinyl-source-stream');
-var pkg = require('./package.json');
-var config = yaml.safeLoad(fs.readFileSync('./config.yml', 'utf8'));
-var reload = browserSync.reload;
-var root = {
-  src:'./src',
-  build:'./gh-pages'
-};
 var banner = [
 '/*!',
 ' * <%= pkg.name %> - <%= pkg.description %>',
@@ -36,6 +9,29 @@ var banner = [
 ' * License : <%= pkg.license %>',
 ' */',
 ''].join('\n');
+var browserify = require('browserify');
+var browserSync = require('browser-sync');
+var cssnext = require('gulp-cssnext');
+var del = require('del');
+var data = require('gulp-data');
+var fm = require('gulp-front-matter');
+var fs = require('fs');
+var gulp = require('gulp');
+var header = require('gulp-header');
+var imagemin = require('gulp-imagemin');
+var jshint = require('gulp-jshint');
+var pkg = require('./package.json');
+var pngquant = require('imagemin-pngquant');
+var reload = browserSync.reload;
+var rename = require('gulp-rename');
+var runSequence = require('run-sequence');
+var source = require('vinyl-source-stream');
+var sourcemaps   = require('gulp-sourcemaps');
+var stylestats = require('gulp-stylestats');
+var stylish = require('jshint-stylish');
+var uglify = require('gulp-uglify');
+var yaml = require('js-yaml');
+var config = yaml.safeLoad(fs.readFileSync('./config.yml', 'utf8'));
 
 // ----------------------------------------------------------------
 
@@ -59,8 +55,8 @@ gulp.task('html', function() {
 				pkg: pkg,
 				site: config
 			},
-			helpers: root.src + '/helpers/**/*.js',
-			partials: root.src + '/partials/**/*.hbs'
+			helpers: './src/helpers/**/*.js',
+			partials: './src/partials/**/*.hbs'
 		}))
 		.pipe(rename(function(path){
 				if (path.basename == 'index'){
@@ -77,48 +73,48 @@ gulp.task('html', function() {
 // ----------------------------------------------------------------
 
 gulp.task('jshint', function() {
-  return gulp.src(root.src + '/**/*.js')
+  return gulp.src('./src/**/*.js')
   .pipe(jshint())
   .pipe(jshint.reporter(stylish));
 });
 
 gulp.task('javascript', function() {
-  return browserify(root.src + '/static/js/' + pkg.name + '.js', { debug: true })
+  return browserify('./src/static/js/' + pkg.name + '.js', { debug: true })
     .bundle()
     .on('error', function (err) {
       console.log('Error : ' + err.message);
       this.emit('end');
     })
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest(root.build + '/js'));
+    .pipe(gulp.dest('./gh-pages/js'));
 });
 
 gulp.task('jsmin', function() {
-	return gulp.src(root.build + '/js/' + pkg.name + '.js')
+	return gulp.src('./gh-pages/js/' + pkg.name + '.js')
     .pipe(uglify())
-		.pipe(gulp.dest(root.build + '/js'));
+		.pipe(gulp.dest('./gh-pages/js'));
 });
 
 // ----------------------------------------------------------------
 
 gulp.task('css', function() {
-	return gulp.src(root.src + '/static/css/' + pkg.name + '.css')
+	return gulp.src('./src/static/css/' + pkg.name + '.css')
 		.pipe(header(banner, {pkg:pkg}))
 		.pipe(cssnext({
         browsers: ['last 2 versions'],
         sourcemap: true
     }))
-		.pipe(gulp.dest(root.build + '/css'));
+		.pipe(gulp.dest('./gh-pages/css'));
 });
 
 gulp.task('cssmin', function() {
-	return gulp.src(root.src + '/static/css/' + pkg.name + '.css')
+	return gulp.src('./src/static/css/' + pkg.name + '.css')
     .pipe(header(banner, {pkg:pkg}))
     .pipe(cssnext({
       browsers: ['last 2 versions'],
 			compress: true
 		}))
-		.pipe(gulp.dest(root.build + '/css'));
+		.pipe(gulp.dest('./gh-pages/css'));
 });
 
 gulp.task('stylestats', function() {
@@ -127,19 +123,19 @@ gulp.task('stylestats', function() {
 			type: 'json',
 			outfile: true
 		}))
-		.pipe(gulp.dest(root.src + '/data'));
+		.pipe(gulp.dest('./src/data'));
 });
 
 // ----------------------------------------------------------------
 
 gulp.task('images', function() {
-	return gulp.src(root.src + '/static/images/**/*')
+	return gulp.src('./src/static/images/**/*')
 		.pipe(imagemin({
 			progressive: true,
 			svgoPlugins: [{removeViewBox: false}],
 			use: [pngquant()]
 		}))
-		.pipe(gulp.dest(root.build + '/images'));
+		.pipe(gulp.dest('./gh-pages/images'));
 });
 
 // ----------------------------------------------------------------
@@ -152,11 +148,11 @@ gulp.task('browsersync', function() {
 		}
 	});
 
-  gulp.watch([root.src + '/static/css/*.css'], ['css']);
-  gulp.watch([root.src + '/static/js/*.js'], ['javascript']);
-  gulp.watch([root.src + '/**/*.hbs'], ['html']);
-  gulp.watch([root.build + '/{css,js}/*.{css,js}']).on('change', reload);
-  gulp.watch([root.build + '/*.html']).on('change', reload);
+  gulp.watch(['./src/static/css/*.css'], ['css']);
+  gulp.watch(['./src/static/js/*.js'], ['javascript']);
+  gulp.watch(['./src/**/*.hbs'], ['html']);
+  gulp.watch(['./gh-pages/{css,js}/*.{css,js}']).on('change', reload);
+  gulp.watch(['./gh-pages/*.html']).on('change', reload);
 });
 
 // ----------------------------------------------------------------
